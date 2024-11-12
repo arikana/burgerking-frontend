f212e1bdc6c3a62e7c03674610553f71dd027b67
Arika <moonjb104605@naver.com>
2024-11-12 20:36:14 +0900

    index

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', async (event) => {
     const budget = document.getElementById('budget').value;
 
     try {
-        const response = await fetch('https://web-production-82df.up.railway.app/', {  // Railway에서 제공된 URL로 수정
+        const response = await fetch('https://web-production-b5a7b.up.railway.app/', {  // Railway에서 제공된 URL로 수정
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
